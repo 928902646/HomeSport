1cb26bff02b98483885654fd239060555c1aeacb
zzqybyb19860112 <417986033@qq.com>
2016-11-09 18:35:58 +0800

    沉浸式

--- app/src/main/java/com/tiyujia/homesport/common/homepage/fragment/HomePageFragment.java
+++ app/src/main/java/com/tiyujia/homesport/common/homepage/fragment/HomePageFragment.java
@@ -1,8 +1,23 @@
 package com.tiyujia.homesport.common.homepage.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tiyujia.homesport.R;
+
 /**
  * Created by zzqybyb19860112 on 2016/10/18.4444
  */
 public class HomePageFragment extends Fragment {
+    private View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.homepage_fragment,null);
+        return view;
+    }
 }
